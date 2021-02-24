0cc7137c1032f2d400581432981136e723bdafdb
Julius Simutis <julius@ivejas.lt>
2021-02-24 21:36:14 +0200

    Add child element

--- paskaita9/main.js
+++ paskaita9/main.js
@@ -20,21 +20,34 @@ let colorChange = 0;
 
 let direction = false;
 
+let offset = 0;
+
 function onButtonClick() {
   const elm = document.getElementById('test');
-  switch (colorChange) {
-    case 0:
-      elm.style.backgroundColor = 'red';
-      break;
-    case 1:
-      elm.style.backgroundColor = 'green';
-      break;
-    case 2:
-      elm.style.backgroundColor = '#faf';
-      break;
-    default:
-      break;
-  }
+
+  const newElem = document.createElement('span');
+
+  newElem.className = 'box';
+  newElem.style.backgroundColor =
+    `#${((1 << 24) * Math.random() | 0).toString(16)}`;
+  newElem.style.top = `${offset}px`;
+
+  offset += 5;
+  elm.appendChild(newElem);
+
+  // switch (colorChange) {
+  //   case 0:
+  //     elm.style.backgroundColor = '#faa';
+  //     break;
+  //   case 1:
+  //     elm.style.backgroundColor = '#afa';
+  //     break;
+  //   case 2:
+  //     elm.style.backgroundColor = '#faf';
+  //     break;
+  //   default:
+  //     break;
+  // }
 
   if (colorChange + 1 === 3 || colorChange <= 0) {
     direction = !direction;
@@ -53,5 +66,7 @@ function onLoadFunction() {
   buttonElm.onclick = onButtonClick;
 
 }
+
+
 
 window.onload = onLoadFunction;
